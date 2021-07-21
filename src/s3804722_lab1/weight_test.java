ce1b13d8d86f9165646b55e29b1903aae622ae0e
Huu Phuoc <s3804722@users.noreply.github.com>
2021-07-21 15:57:51 +0700

    junit

---
+++ src/s3804722_lab1/weight_test.java
@@ -0,0 +1,15 @@
+package s3804722_lab1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class weight_test {
+
+	@Test
+	void test() {
+//		fail("Not yet implemented");
+		assertEquals((float)2, SomeMethods.weight_index(8, 2), 0.0f);
+	}
+
+}
